51faf4021e66fe26c86decff0ea207a4d630c5b7
Dmitiy Sukhov <dstujev@gmail.com>
2025-05-08 19:02:26 +0300

    add eventEmitter

--- objects-and-functions/eventEmitter.js
+++ objects-and-functions/eventEmitter.js
@@ -1,9 +1,46 @@
 /* Напишите реализацию EventEmitter. EventEmiitter - это класс, который позволяет
 добавлять и удалять обработчики событий, а также выпускать (emit) события */
 
-class EventEmitter {}
+class EventEmitter {
+  events = new Map();
+
+  on(evtName, fn) {
+    const event = this.events.get(evtName);
+
+    if (event) {
+      event.push(fn);
+    } else {
+      this.events.set(evtName, [fn]);
+    }
+
+    return this;
+  }
+
+  emit(evtName) {
+    this.events.get(evtName)?.forEach((fn) => {
+      fn();
+    });
+
+    return this;
+  }
+
+  pop(evtName) {
+    const event = this.events.get(evtName);
+
+    if (event) {
+      event.pop();
+
+      if (event.length === 0) {
+        this.events.delete(evtName);
+      }
+    }
+
+    return this;
+  }
+}
 
 const ee = new EventEmitter();
+
 const cb1 = () => console.log("cb1");
 const cb2 = () => console.log("cb2");
 
