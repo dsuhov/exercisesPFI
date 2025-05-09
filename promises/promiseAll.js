11b5f347f01313bfbea312fb2e8bd026ff2c1e72
Dmitiy Sukhov <dstujev@gmail.com>
2025-05-09 08:54:49 +0300

    added promiseAll

---
+++ promises/promiseAll.js
@@ -0,0 +1,46 @@
+Promise.myAll = (promises) => {
+  return new Promise((resolve, reject) => {
+    let count = 0;
+    const results = [];
+
+    promises.forEach((promise, idx) => {
+      promise
+        .then((result) => {
+          results[idx] = result;
+          count++;
+
+          if (count === promises.length) {
+            resolve(results);
+          }
+        })
+        .catch((e) => {
+          reject(new Error());
+        });
+    });
+  });
+};
+
+const promise1 = Promise.resolve(3);
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 1000, "wewewewe");
+});
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 400, "foo");
+});
+
+Promise.myAll([promise1, promise2, promise3])
+  .then((values) => {
+    console.log(values);
+  })
+  .catch((e) => console.log(e));
+
+const promise4 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 1000, "promise4");
+});
+const promise5 = Promise.reject("error");
+
+Promise.myAll([promise4, promise5])
+  .then((values) => {
+    console.log(values);
+  })
+  .catch((e) => console.log("error"));
